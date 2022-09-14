7e1e49829e36f272c6181eb8db08cbf218d54ca3
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2022-09-14 18:32:32 +0530

    Removed one test case form login

--- src/test/java/com/actitime/tests/Login.java
+++ src/test/java/com/actitime/tests/Login.java
@@ -33,12 +33,7 @@ public class Login extends BaseClass {
 			Assert.assertTrue(result, "Could not loggin to actitime...");				
 		}	
 	
-	@Test
-	public static void login_003() throws IOException
-	{							
-		boolean result = CommonUtils.loginToActiTimeApplication();		
-		Assert.assertTrue(result, "Could not loggin to actitime...");				
-	}	
+		
 	
 	
 	
